8599c8cb495ca7c2e97bf73eb267a5c87f8a1811
Veljko <veljkozajecar@gmail.com>
2022-05-22 10:20:19 +0200

    Back project initialized

---
+++ mackolec/src/main/java/com/vet/mackolec/MackolecApplication.java
@@ -0,0 +1,13 @@
+package com.vet.mackolec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MackolecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MackolecApplication.class, args);
+	}
+
+}
---
+++ mackolec/src/test/java/com/vet/mackolec/MackolecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.mackolec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MackolecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
